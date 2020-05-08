9c4ef3e75c17561847d5ec11e20e6a6dade74551
noker006 <48758950+noker006@users.noreply.github.com>
2020-05-08 22:20:26 +0300

    newtask1

--- src/aura/TimeDateCompanent/TimeDateCompanentHelper.js
+++
@@ -1,9 +0,0 @@
-({
-Show : function(messageText, titleText) {
-    var toastEvent = $A.get("e.force:showToast");
-    toastEvent.setParams({
-    "title": titleText,
-    "message": messageText});
-    toastEvent.fire();
-}
-})
